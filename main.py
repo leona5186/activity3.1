0ee4970b4eb6d0fee486a12889820af47fc9d3fd
leona5186 <leona5186workmail@gmail.com>
2025-05-16 23:20:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ df_clean.to_csv('data/processed_dataset.csv', index=False)
 
 print("Duplicates removed successfully.")
 
+
+import shutil
+
+# Load and clean
+df = pd.read_csv('data/raw_dataset.csv')
+df_clean = df.drop_duplicates()
+df_clean.to_csv('data/processed_dataset.csv', index=False)
+
+# Optional: Copy it outside data folder to make it visible in root directory (GitHub shows it easier)
+shutil.copy('data/processed_dataset.csv', 'processed_dataset.csv')
+
+print("Dataset cleaned and copied successfully.")
+
+
 # This is a trigger for GitHub Actions
 # trigger CI/CD workflow
 
